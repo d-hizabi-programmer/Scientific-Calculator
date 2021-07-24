4dae2755c39c30d6246f820423ea231c792a435e
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-24 14:13:08 +0530

    Number 2 to 9 buttons click event done!

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
-function clear() {
-  $("values").val("");
+function clearEntries(evt) {
+  $("#display").val("0");
+  console.log("Entries Cleared!");
+  evt.preventDefault();
 }
 
 //values click event handler
-function values(number) {
-  $("#values").val(number);
+function inputValues(evt, number) {
+  var currentValue = $("#display").val();
+  evt.preventDefault();
+  if (currentValue == 0) {
+    $("#display").val(number);
+  } else {
+    var newValue = currentValue + number;
+    $("#display").val(newValue);
+  }
+  console.log("Value added succesfully!");
 }
 
 $(document).ready(function () {
-  $("#clear").on(clear); //for clearing entries
+  $("#display").focus();
 
-  //click event of textbox
-  $("#values").click(function () {
-    $("#values").val("");
+  //for clearing entries
+  $("#clear").on("click", clearEntries);
+  console.log("Value is:" + $("#display").val());
+
+  //numer 2 click event
+  $("#numTwo").on("click", (evt) => {
+    inputValues(evt, 2);
   });
 
-  $("#numSeven").on(values(7));
-  $("#numEight").on(values(8));
-  $("#numNine").on(values(9));
+  //numer 3 click event
+  $("#numThree").on("click", (evt) => {
+    inputValues(evt, 3);
+  });
+
+  //numer 4 click event
+  $("#numFour").on("click", (evt) => {
+    inputValues(evt, 4);
+  });
+
+  //numer 5 click event
+  $("#numFive").on("click", (evt) => {
+    inputValues(evt, 5);
+  });
+
+  //numer 6 click event
+  $("#numSix").on("click", (evt) => {
+    inputValues(evt, 6);
+  });
+
+  //number 7 click event
+  $("#numSeven").on("click", (evt) => {
+    inputValues(evt, 7);
+  });
+
+  //numer 8 click event
+  $("#numEight").on("click", (evt) => {
+    inputValues(evt, 8);
+  });
+
+  //numer 9 click event
+  $("#numNine").on("click", (evt) => {
+    inputValues(evt, 9);
+  });
 });
