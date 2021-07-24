fe274d7909c9041ef86730bca7f5d764ef24fbe3
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-24 13:04:50 +0530

    button 7,8 and 9 evethandlers!

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ function clear() {
   $("values").val("");
 }
 
-$(document).ready(function () {
-  $("#clear").on(clear);
+//values click event handler
+function values(number) {
+  $("#values").val(number);
+}
 
+$(document).ready(function () {
+  $("#clear").on(clear); //for clearing entries
+
+  //click event of textbox
   $("#values").click(function () {
     $("#values").val("");
   });
+
+  $("#numSeven").on(values(7));
+  $("#numEight").on(values(8));
+  $("#numNine").on(values(9));
 });
