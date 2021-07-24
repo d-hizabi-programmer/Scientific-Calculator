9fc86659312bf9abf765bf96cd31b433b80d70f2
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-24 12:44:29 +0530

    textbox onclick and clear button event handler completed!

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function clear() {
+  $("values").val("");
+}
+
 $(document).ready(function () {
   $("#clear").on(clear);
+
+  $("#values").click(function () {
+    $("#values").val("");
+  });
 });
